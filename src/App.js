04a5e3c772efc1de1e01d515a7f1a8d71ce46960
Paul Petrov <paul@bargamut.ru>
2020-10-27 16:56:37 +0300

    Update App component for test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
+import { Layout } from 'antd';
+
 import logo from './logo.svg';
 
 import './App.css';
 
+const { Header, Footer, Sider, Content } = Layout;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<Layout className="site">
+			<Sider className="sider">
+				<img src={logo} alt=""/>
+			</Sider>
+			
+			<Layout>
+				<Header className="header">Ant Design Learning</Header>
+
+				<Content className="main">Content</Content>
+				
+				<Footer>&copy; Bargamut 2020</Footer>
+			</Layout>
+		</Layout>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { ConfigProvider, Button } from 'antd';
+import { ConfigProvider } from 'antd';
 import ruRU from 'antd/lib/locale/ru_RU';
 
 ReactDOM.render(
   <React.StrictMode>
-		<ConfigProvider locale={ruRU}>
-			<Button type="primary">Button</Button>
-		
+		<ConfigProvider locale={ruRU}>		
 			<App />
 		</ConfigProvider>
   </React.StrictMode>,
